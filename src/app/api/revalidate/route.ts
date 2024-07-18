4a2d710e397ce62c9bf084c75f8d138ca5bd88c5
sheddyboy <shedrackitulah@gmail.com>
2024-07-18 17:03:53 +0100

    feat: change runtime to nodejs

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export async function POST(req: NextRequest) {
   revalidatePath("/api/plans");
